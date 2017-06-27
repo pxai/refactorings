815b2298fb23d7175cc35e67283d9461db11b74e
pxai <pello_altadill@cuatrovientos.org>
2017-06-27 12:33:02 +0200

    Ok, done with Encapsulation

---
+++ src/main/java/io/pello/refactorings/encapsulate/CustomerRefactored.java
@@ -0,0 +1,42 @@
+package io.pello.refactorings.encapsulate;
+
+/**
+ * Rename variables and methods
+ * This class is intended to be used as a refactoring playground
+ * @author PELLO_ALTADILL
+ */
+public class CustomerRefactored {
+
+		private String name;
+		private int id;
+		
+		public CustomerRefactored () {
+			init();
+		}
+		
+		private void init () {
+			setName("Eugene Krabs");
+			setId(42);
+		}
+		
+		public String toString() {
+			return getId() + ":" + getName();  
+		}
+
+		String getName() {
+			return name;
+		}
+
+		void setName(String name) {
+			this.name = name;
+		}
+
+		int getId() {
+			return id;
+		}
+
+		void setId(int id) {
+			this.id = id;
+		}
+
+}
