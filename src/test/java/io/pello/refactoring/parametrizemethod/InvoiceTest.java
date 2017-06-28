f3709dac4e5772dbefad44d6ecc25b707ca484dd
pxai <pello_altadill@cuatrovientos.org>
2017-06-28 23:44:50 +0200

    Ok, its done with parametrize method

---
+++ src/test/java/io/pello/refactoring/parametrizemethod/InvoiceTest.java
@@ -0,0 +1,36 @@
+package io.pello.refactoring.parametrizemethod;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InvoiceTest {
+
+	@Test
+	public void testChargeNormal() {
+		Invoice invoice = new Invoice(100,new Customer(20, false));
+		float expected = 100f;
+		float actual = invoice.charge();
+		
+		assertEquals("Test charge for underage", actual, expected, 0.01f);
+	}
+	
+	@Test
+	public void testChargeUnderage() {
+		Invoice invoice = new Invoice(100,new Customer(17, false));
+		float expected = 50f;
+		float actual = invoice.charge();
+		
+		assertEquals("Test charge for underage", actual, expected, 0.01f);
+	}
+	
+	@Test
+	public void testChargeCash() {
+		Invoice invoice = new Invoice(100,new Customer(19, true));
+		float expected = 80f;
+		float actual = invoice.charge();
+		
+		assertEquals("Test charge for underage", actual, expected, 0.01f);
+	}
+
+}
