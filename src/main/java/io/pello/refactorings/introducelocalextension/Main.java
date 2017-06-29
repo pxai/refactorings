d9ebdf845b78cfc183a940ab704cc4233666b1e8
pxai <pello_altadill@cuatrovientos.org>
2017-06-29 19:25:19 +0200

    Introduce local extension added

---
+++ src/main/java/io/pello/refactorings/introducelocalextension/Main.java
@@ -0,0 +1,15 @@
+package io.pello.refactorings.introducelocalextension;
+
+/**
+ * Introduce Local Extension
+ * This class is intended to be used as a refactoring playground
+ * @author PELLO_ALTADILL
+ */
+public class Main {
+	private Conversor conversor = new Conversor();
+	
+	public double convert (double amount) {
+		return conversor.euro2Dollar(amount);
+	}
+
+}
